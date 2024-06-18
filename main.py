2b55528ed83f6aaf382b5be231a4d2ec72521246
szu-sg <54049847+szu-sg@users.noreply.github.com>
2024-06-18 07:02:41 +0000

    上传

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
-    print("Hello World!")
+    print(1)
+    for i in range(1, 10):
+        for j in range(1, i+1):
+            print(f"{j} * {i} = {i*j}", end="\t")
+        print()
